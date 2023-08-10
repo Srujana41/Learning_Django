2c2acc71abf45a51584c76b779da169a23df4166
Srujana41 <sabbanisrujana41@gmail.com>
2023-08-10 15:49:46 +0530

    delete and edit functionality done

--- ToDo/todo/views.py
+++ ToDo/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404, render
 from django.http import HttpResponse
 from .models import Task
 
@@ -11,4 +11,35 @@ def addTask(request):
     #to create task in database by sending task entered by user
     Task.objects.create(task=task)
     #redirect user back to home page after clicking on add button
+    return redirect('home')
+
+def markAsDone(request, pk):
+    # mark task as done
+    task = get_object_or_404(Task, pk=pk)
+    task.is_Completed = True
+    task.save()
+    return redirect('home')
+
+def markAsUndone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_Completed = False
+    task.save()
+    return redirect('home')
+
+def editTask(request, pk):
+    task_get = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        task_get.task = new_task
+        task_get.save()
+        return redirect('home')
+    else:
+        context ={
+            'task_get':task_get
+        }
+        return render(request, "edit-task.html", context)
+    
+def deleteTask(request, pk):
+    task= get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('home')
--- ToDo/todo_main/views.py
+++ ToDo/todo_main/views.py
@@ -4,7 +4,10 @@ from todo.models import Task
 def home(request):
     # To get data with filter and then arrange data in ascending order add "-" to the field otherwise nothing for descending order
     tasks = Task.objects.filter(is_Completed = False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_Completed = True).order_by('-created_at')
+    # print(completed_tasks)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks
     }
     return render(request, 'home.html', context)
